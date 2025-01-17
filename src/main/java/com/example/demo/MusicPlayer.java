118cff1da9aa5d78a6eda90fb3789234af1336f4
Vihan Agarwal <vihan.agarwal@CTMCWGR972.local>
2025-01-17 11:01:40 +0530

    everthing working dates added

--- src/main/java/com/example/demo/MusicPlayer.java
+++ src/main/java/com/example/demo/MusicPlayer.java
@@ -68,69 +68,75 @@ class Song{
         title=name;
     }
 }
-//class Dates{
-//    LocalDate myObj;
-//    HashMap<LocalDate,HashMap<Song, Integer> > map = new HashMap<>();//date ,top10songs
-//    HashMap<LocalDate,Song> minimummap=new HashMap<>();
-//    HashMap<LocalDate,Integer> minimumcount=new HashMap<>();
-//    public Dates(){
-//        myObj = LocalDate.now();
-//    }
-//    public void nextDay(){
-//        System.out.println("local date earlier was "+myObj);
-//        myObj=myObj.plusDays(1);
-//        System.out.println("local date now is "+myObj);
-//    }
-//    public void gettop10songsdate(LocalDate d){
-//        for(Song it:map.get(d).keySet()){
-//            System.out.println(it.title +" - "+it.playCount);
-//        }
-//    }
-//    public void playsongdate(LocalDate d,Song songname){
-//        HashMap<Song,Integer> top10songs=map.get(d);
-//
-//        Integer minncount=minimumcount.get(d);
-//        Song minimumtopsong=minimummap.get(d);
-//        if(top10songs.size()<10) {
-//            if (top10songs.containsKey(songname)) {
-//                top10songs.put(songname, top10songs.get(songname) + 1);
-//                if(minimumtopsong==songname) minncount++;
-//            } else {
-//                top10songs.put(songname, songname.playCount);
-//                if(songname.playCount>minncount){
-//                    minncount=Integer.MAX_VALUE;
-//                    minimumtopsong=null;
-//                    for(Song it:top10songs.keySet()){
-//                        if(minncount>it.playCount){
-//                            minncount=it.playCount;
-//                            minimumtopsong=songname;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        else if(songname.playCount>minncount){
-//            //already 10 songs are there
-//            //need to find new minimum
-//            top10songs.remove(minimumtopsong);
-//            top10songs.put(songname,songname.playCount);
-//            minncount=Integer.MAX_VALUE;
-//            minimumtopsong=null;
-//            for(Song it:top10songs.keySet()){
-//                if(minncount>it.playCount){
-//                    minncount=it.playCount;
-//                    minimumtopsong=songname;
-//                }
-//            }
-//        }
-//        map.put(d,top10songs);
-//        minimumcount.put(d,minncount);
-//        minimummap.put(d,minimumtopsong);
-//    }
-//
-//}
+class Dates{
+    LocalDate myObj;
+    HashMap<LocalDate, HashMap<Song, Integer>> map = new HashMap<>(); // date, top10songs
+    HashMap<LocalDate, Song> minimummap = new HashMap<>();
+    HashMap<LocalDate, Integer> minimumcount = new HashMap<>();
+
+    public Dates() {
+        myObj = LocalDate.now();
+    }
+
+    public void nextDay() {
+        System.out.println("local date earlier was " + myObj);
+        myObj = myObj.plusDays(1);
+        System.out.println("local date now is " + myObj);
+    }
+
+    public void gettop10songsdate(LocalDate d) {
+        if (map.containsKey(d)) {
+            for (Song it : map.get(d).keySet()) {
+                System.out.println(it.title + " - " + it.playCount);
+            }
+        } else {
+            System.out.println("No songs played on this date.");
+        }
+    }
+
+    public void playsongdate(LocalDate d, Song songname) {
+        map.putIfAbsent(d, new HashMap<>());
+        HashMap<Song, Integer> top10songs = map.get(d);
+
+        Integer minncount = minimumcount.getOrDefault(d, Integer.MAX_VALUE);
+        Song minimumtopsong = minimummap.get(d);
+
+        if (top10songs.size() < 10) {
+            if (top10songs.containsKey(songname)) {
+                top10songs.put(songname, top10songs.get(songname) + 1);
+                if (minimumtopsong == songname) minncount++;
+            } else {
+                top10songs.put(songname, songname.playCount);
+                if (songname.playCount > minncount) {
+                    minncount = Integer.MAX_VALUE;
+                    minimumtopsong = null;
+                    for (Song it : top10songs.keySet()) {
+                        if (minncount > it.playCount) {
+                            minncount = it.playCount;
+                            minimumtopsong = it;
+                        }
+                    }
+                }
+            }
+        } else if (songname.playCount > minncount) {
+            top10songs.remove(minimumtopsong);
+            top10songs.put(songname, songname.playCount);
+            minncount = Integer.MAX_VALUE;
+            minimumtopsong = null;
+            for (Song it : top10songs.keySet()) {
+                if (minncount > it.playCount) {
+                    minncount = it.playCount;
+                    minimumtopsong = it;
+                }
+            }
+        }
+        map.put(d, top10songs);
+        minimumcount.put(d, minncount);
+        minimummap.put(d, minimumtopsong);
+    }
+}
 public class MusicPlayer{
-//    Dates date=new Dates();
+    Dates date=new Dates();
     HashMap<Song,Integer> top10songs=new HashMap<>();//wrt total count
     HashMap<Song,Artist> songs=new HashMap<>();
     int minncount=Integer.MAX_VALUE;
@@ -179,7 +185,7 @@ public class MusicPlayer{
         if(songname==null) return;
         songname.playCount++;
         songs.get(songname).playsongartist(songname);
-//        date.playsongdate(date.myObj,songname);
+        date.playsongdate(date.myObj,songname);
         if (top10songs.size() < 10) {
             if (top10songs.containsKey(songname)) {
                 top10songs.put(songname, top10songs.get(songname) + 1);
@@ -209,12 +215,12 @@ public class MusicPlayer{
         Artist artist = findingmap2.get(artistName);
         artist.retreive();
     }
-//    public void GotoNextDay(){
-//        date.nextDay();
-//    }
-//    public void gettop10songbydate(LocalDate d){
-//        date.gettop10songsdate(d);
-//    }
+    public void GotoNextDay(){
+        date.nextDay();
+    }
+    public void gettop10songbydate(LocalDate d){
+        date.gettop10songsdate(d);
+    }
 //    public void PrintSonglist(List<Song> l){
 //        int n= l.size();
 //        for(int i=0;i<n;i++){
